c4972a432415135a4f6c24b01165090fb5151a3a
Cameron Custer <custer.cameron@gmail.com>
2024-05-15 15:13:04 -0400

    luke nits

--- src/rmq.rs
+++ src/rmq.rs
@@ -7,6 +7,7 @@ impl<T: Ord + Copy> RMQ<T> {
         let mut i = 0;
         while (2 << i) <= a.len() {
             let mut new_row = vec![];
+            new_row.reserve(t[i].len() - (1 << i));
             for j in 0..t[i].len() - (1 << i) {
                 new_row.push(t[i][j].min(t[i][j + (1 << i)]));
             }
@@ -17,8 +18,7 @@ impl<T: Ord + Copy> RMQ<T> {
     }
 
     pub fn query(&self, range: std::ops::Range<usize>) -> T {
-        let len = range.end - range.start;
-        let lg = len.ilog2() as usize;
+        let lg = range.len().ilog2() as usize;
         self.t[lg][range.start].min(self.t[lg][range.end - (1 << lg)])
     }
 }
