630de75457f8f3e4385c24d5c5bd1c3966154885
Cameron Custer <custer.cameron@gmail.com>
2024-05-15 15:09:37 -0400

    fix rmq query

--- src/rmq.rs
+++ src/rmq.rs
@@ -18,10 +18,7 @@ impl<T: Ord + Copy> RMQ<T> {
 
     pub fn query(&self, range: std::ops::Range<usize>) -> T {
         let len = range.end - range.start;
-        let mut lg = 0;
-        while (2 << lg) <= len {
-            lg += 1;
-        }
+        let lg = len.ilog2() as usize;
         self.t[lg][range.start].min(self.t[lg][range.end - (1 << lg)])
     }
 }
