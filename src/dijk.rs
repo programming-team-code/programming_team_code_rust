1b260b8f055793dab5b7f20ac24a29fa8778b661
Cameron Custer <custer.cameron@gmail.com>
2024-05-15 13:09:27 -0400

    add dijk

---
+++ src/dijk.rs
@@ -0,0 +1,19 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
+pub fn dijk(adj: &Vec<Vec<(usize, u64)>>, s: usize) -> Vec<u64> {
+    let n = adj.len();
+    let mut dist = vec![std::u64::MAX; n];
+    let mut q = BinaryHeap::new();
+    q.push(Reverse((0, s)));
+    while let Some(Reverse((d, u))) = q.pop() {
+        if dist[u] <= d {
+            continue;
+        }
+        dist[u] = d;
+        for &(v, w) in &adj[u] {
+            q.push(Reverse((dist[u] + w, v)));
+        }
+    }
+    dist
+}
