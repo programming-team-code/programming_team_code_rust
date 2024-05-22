8cb0ae41e6fc4d363db0b9e8eb9d923edcc89f79
Cameron Custer <custer.cameron@gmail.com>
2024-05-22 09:56:08 -0400

    add fenwick tree aizu test

---
+++ examples/range_sum_query.rs
@@ -0,0 +1,21 @@
+// verification-helper: PROBLEM https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_B
+
+use proconio::input;
+use programming_team_code_rust::fenwick::Fenwick;
+
+fn main() {
+    input! {
+        n: usize,
+        q: usize,
+        queries: [(usize, usize, usize); q],
+    }
+
+    let mut fenwick = Fenwick::<usize>::new(n);
+    for que in queries {
+        if que.0 == 0 {
+            fenwick.add(que.1 - 1, que.2);
+        } else {
+            println!("{}", fenwick.sum(que.1 - 1..que.2));
+        }
+    }
+}
