32b6d36a7d9dc7cde9ead5e6879888442f5b0a36
Cameron Custer <custer.cameron@gmail.com>
2024-05-29 09:23:54 -0400

    fix doctest

--- src/graphs/two_sat.rs
+++ src/graphs/two_sat.rs
@@ -4,13 +4,14 @@ use crate::graphs::scc::get_sccs;
 
 /// # Example
 /// ```
-/// use programming_team_code_rust::graph::two_sat::TwoSat;
+/// use programming_team_code_rust::graphs::two_sat::TwoSat;
+///
 /// let mut ts = TwoSat::new(2);
 /// ts.add_var();
 /// ts.add_clause(0, false, 1, true);
 /// ts.add_clause(1, false, 2, true);
 /// ts.add_clause(2, false, 0, true);
-/// assert_eq!(ts.solve(), Some(vec![true, true, true]));
+/// assert_eq!(ts.solve(), Some(vec![false, false, false]));
 /// ```
 pub struct TwoSat {
     adj: Vec<Vec<usize>>,
