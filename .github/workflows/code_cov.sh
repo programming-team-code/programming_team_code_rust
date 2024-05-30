03ee51064cda138816d12e695b813efdf5ff62e3
Luke Videckis <lukevideckis@gmail.com>
2024-05-30 01:11:46 -0700

    CI yml file to run script instead

--- .github/workflows/code_cov.sh
+++ .github/workflows/code_cov.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # RUSTFLAGS="-C instrument-coverage -C link-dead-code" oj-verify run --tle 20 --jobs 4 examples/data_structures/rmq.rs
 
-set -o pipefail
+set -eo pipefail
 
 executables=$(find target/debug/examples/ -type f -executable | sed 's/^/--object /')
 cov_flags=(-Xdemangler=rustfilt --ignore-filename-regex='/.cargo/registry' --instr-profile=ptc_rust.profdata "${executables}")
