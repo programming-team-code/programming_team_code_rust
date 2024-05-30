87ffb5228bb899d16191697cad371b598a93673a
Luke Videckis <lukevideckis@gmail.com>
2024-05-30 01:41:45 -0700

    fix script again

--- .github/workflows/code_cov.sh
+++ .github/workflows/code_cov.sh
@@ -3,9 +3,9 @@
 
 set -eo pipefail
 
-executables=$(find target/debug/examples/ -type f -executable | sed 's/^/--object /' | tr '\n' ' ')
-cov_flags=(-Xdemangler=rustfilt --ignore-filename-regex='/.cargo/registry' --instr-profile=ptc_rust.profdata "${executables}")
+executables=$(find target/debug/examples/ -type f -executable | sed 's/^/--object /')
+cov_flags="-Xdemangler=rustfilt --ignore-filename-regex='/.cargo/registry' --instr-profile=ptc_rust.profdata $executables"
 
 cargo profdata -- merge -sparse default_*.profraw -o ptc_rust.profdata
-cargo cov -- show "${cov_flags[@]}" --use-color
-cargo cov -- export "${cov_flags[@]}" --summary-only | python .github/workflows/check_code_cov.py
+cargo cov -- show $cov_flags --use-color
+cargo cov -- export $cov_flags --summary-only | python .github/workflows/check_code_cov.py
