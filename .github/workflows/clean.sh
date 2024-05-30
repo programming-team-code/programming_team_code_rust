846775e156131adfcb1b5c29fa07a6d2a91fd238
Luke Videckis <lrvideckis@gmail.com>
2024-05-30 07:14:18 -0700

    Code cov script (#45)
    * adding script
    
    * run new script
    
    * change permissions
    
    * add script
    
    * update script
    
    * fix errors in script
    
    * CI yml file to run script instead
    
    * add clean script
    
    * more specific file name
    
    * nit
    
    * fix bug
    
    * fix script again
    
    * revert again
    
    * now works
    
    ---------
    
    Co-authored-by: Luke Videckis <lukevideckis@gmail.com>

---
+++ .github/workflows/clean.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cargo clean
+rm -rf default_*.profraw ptc_rust.profdata .verify-helper/
---
+++ .github/workflows/code_cov.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# RUSTFLAGS="-C instrument-coverage -C link-dead-code" oj-verify run --tle 20 --jobs 4 examples/data_structures/rmq.rs
+
+set -eo pipefail
+
+executables=$(find target/debug/examples/ -type f -executable | sed 's/^/--object /')
+cov_flags=(-Xdemangler=rustfilt --ignore-filename-regex='/.cargo/registry' --instr-profile=ptc_rust.profdata "${executables}")
+
+cargo profdata -- merge -sparse default_*.profraw -o ptc_rust.profdata
+# shellcheck disable=SC2068 # double quotes cause command to fail, not sure why
+cargo cov -- show ${cov_flags[@]} --use-color
+# shellcheck disable=SC2068
+cargo cov -- export ${cov_flags[@]} --summary-only | python .github/workflows/check_code_cov.py
