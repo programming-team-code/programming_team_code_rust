88cfc68a695d3a30a04eba49d073ab968f7d4741
Luke Videckis <lukevideckis@gmail.com>
2024-05-30 01:55:28 -0700

    now works

--- .github/workflows/code_cov.sh
+++ .github/workflows/code_cov.sh
@@ -7,5 +7,7 @@ executables=$(find target/debug/examples/ -type f -executable | sed 's/^/--objec
 cov_flags=(-Xdemangler=rustfilt --ignore-filename-regex='/.cargo/registry' --instr-profile=ptc_rust.profdata "${executables}")
 
 cargo profdata -- merge -sparse default_*.profraw -o ptc_rust.profdata
-cargo cov -- show "${cov_flags[@]}" --use-color
-cargo cov -- export "${cov_flags[@]}" --summary-only | python .github/workflows/check_code_cov.py
+# shellcheck disable=SC2068 # double quotes cause command to fail, not sure why
+cargo cov -- show ${cov_flags[@]} --use-color
+# shellcheck disable=SC2068
+cargo cov -- export ${cov_flags[@]} --summary-only | python .github/workflows/check_code_cov.py
