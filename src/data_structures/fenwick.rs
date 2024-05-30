edeb76c1ddcf6fdcd25852a9eaddbde554854014
Luke Videckis <lukevideckis@gmail.com>
2024-05-30 09:05:23 -0700

    revert

--- src/data_structures/fenwick.rs
+++ src/data_structures/fenwick.rs
@@ -21,7 +21,7 @@ impl<T: Clone + Default + std::ops::AddAssign<T>> Fenwick<T> {
     /// - Time: O(n)
     /// - Space: O(n)
     pub fn new(n: usize) -> Self {
-        Self {
+        Fenwick {
             ary: vec![T::default(); n],
         }
     }
