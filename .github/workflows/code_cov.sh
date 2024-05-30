49d64c7fcf4b4868eb0a0a8e1442894c44a95e2d
Luke Videckis <lukevideckis@gmail.com>
2024-05-30 01:09:09 -0700

    fix errors in script

--- .github/workflows/code_cov.sh
+++ .github/workflows/code_cov.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
 # RUSTFLAGS="-C instrument-coverage -C link-dead-code" oj-verify run --tle 20 --jobs 4 examples/data_structures/rmq.rs
 
-cargo profdata -- merge -sparse default_*.profraw -o ptc_rust.profdata
+set -o pipefail
 
 executables=$(find target/debug/examples/ -type f -executable | sed 's/^/--object /')
-cov_flags="-Xdemangler=rustfilt --ignore-filename-regex='/.cargo/registry' --instr-profile=ptc_rust.profdata $executables"
+cov_flags=(-Xdemangler=rustfilt --ignore-filename-regex='/.cargo/registry' --instr-profile=ptc_rust.profdata "${executables}")
 
-cargo cov -- show $cov_flags --use-color
-cargo cov -- export $cov_flags --summary-only | python .github/workflows/check_code_cov.py
+cargo profdata -- merge -sparse default_*.profraw -o ptc_rust.profdata
+cargo cov -- show "${cov_flags[@]}" --use-color
+cargo cov -- export "${cov_flags[@]}" --summary-only | python .github/workflows/check_code_cov.py
