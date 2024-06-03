ff39a9ddf9cd303860692dc68dc2a7c5a661be73
Luke Videckis <lukevideckis@gmail.com>
2024-06-02 18:26:32 -0700

    Revert "simplify test cuz I found a better test"
    This reverts commit 4ab8679028f4edc95c018c925738fb5328cdfded.

--- examples/graphs/hld_subtree_sum_yosupo.rs
+++ examples/graphs/hld_subtree_sum_yosupo.rs
@@ -20,11 +20,13 @@ fn main() {
         adj[p].push(c);
     }
 
-    let hld = HLD::new(&mut adj, false);
+    let hld_nodes = HLD::new(&mut adj, false);
     let mut fenwick = Fenwick::<usize>::new(n);
 
+    let hld_edges = HLD::new(&mut adj, true);
+
     for (i, &elem) in a.iter().enumerate() {
-        fenwick.add(hld.tin[i], elem);
+        fenwick.add(hld_nodes.tin[i], elem);
     }
 
     for _ in 0..q {
@@ -38,13 +40,19 @@ fn main() {
                     u: usize,
                     delta: usize,
                 }
-                fenwick.add(hld.tin[u], delta);
+                fenwick.add(hld_nodes.tin[u], delta);
             }
             _ => {
                 input! {
                     u: usize,
                 }
-                println!("{}", fenwick.sum(hld.sub_tree(u)));
+                let nodes_subtree = hld_nodes.sub_tree(u);
+                let edges_subtree = hld_edges.sub_tree(u);
+                assert!(
+                    nodes_subtree.start + 1 == edges_subtree.start
+                        && nodes_subtree.end == edges_subtree.end
+                );
+                println!("{}", fenwick.sum(nodes_subtree));
             }
         }
     }
