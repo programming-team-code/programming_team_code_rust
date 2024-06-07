883ff8a542a78f053f5a4f849dfa15681b0a515b
Luke Videckis <lukevideckis@gmail.com>
2024-06-07 10:14:32 -0700

    simplify

--- examples/strings/cmp_substrs.rs
+++ examples/strings/cmp_substrs.rs
@@ -1,24 +1,30 @@
-// verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/lesson/8/ITP2/all/ITP2_5_A
+// verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/lesson/8/ITP2/all/ITP2_3_D
 
 use proconio::input;
-use programming_team_code_rust::helpers::compress::compress;
 use programming_team_code_rust::strings::suffix_array::SufAry;
 
 fn main() {
     input! {
         n: usize,
-        a: [i32; 2 * n]
+        mut a: [usize; n],
+        m: usize,
+        b: [usize; m]
     }
 
-    let (compressed, max_val) = compress::<i32>(&a);
+    let are_equal = a == b;
 
-    let suf_ary = SufAry::new(&compressed, max_val);
+    a.extend(b);
 
-    let mut indexes = (0..n).collect::<Vec<usize>>();
+    let suf_ary = SufAry::new(&a, 1000);
+    let res = suf_ary.cmp_substrs(0..n, n..n + m);
 
-    indexes.sort_by(|&i1, &i2| suf_ary.cmp_substrs(2 * i1..2 * i1 + 2, 2 * i2..2 * i2 + 2));
-
-    for i in indexes {
-        println!("{} {}", a[2 * i], a[2 * i + 1]);
+    if are_equal {
+        assert_eq!(res, std::cmp::Ordering::Equal);
+        println!("0");
+    } else {
+        println!(
+            "{}",
+            (suf_ary.cmp_substrs(0..n, n..n + m) == std::cmp::Ordering::Less) as usize
+        );
     }
 }
