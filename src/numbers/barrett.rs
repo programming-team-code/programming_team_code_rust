5d323513de4a65b79ca1e119b8a5039094c31be4
Luke Videckis <lukevideckis@gmail.com>
2024-06-14 14:21:57 -0500

    finish docs

--- src/numbers/barrett.rs
+++ src/numbers/barrett.rs
@@ -1,11 +1,30 @@
-//! # Barrett Mod
+//! # Barrett Division and Remainder
 
+/// See <https://github.com/kth-competitive-programming/kactl/blob/main/content/various/FastMod.h>
+///
+/// # Example
+/// ```
+/// use programming_team_code_rust::numbers::barrett::Barrett;
+/// let barr = Barrett::new(6_u32);
+/// assert_eq!(barr.div(20_u64), (3, 2));
+/// ```
+///
+/// # Panics
+/// ```panic
+/// use programming_team_code_rust::numbers::barrett::Barrett;
+/// let barr = Barrett::new(0_u32);
+/// ```
 pub struct Barrett {
     denom: u32,
     im: u64,
 }
 
 impl Barrett {
+    /// Creates a new Barrett struct
+    ///
+    /// # Complexity
+    /// - Time: O(1)
+    /// - Space: O(1)
     pub fn new(denom: u32) -> Self {
         assert_ne!(denom, 0);
         Self {
@@ -14,6 +33,11 @@ impl Barrett {
         }
     }
 
+    /// Gets quotient and remainder of numer / denom
+    ///
+    /// # Complexity
+    /// - Time: O(1)
+    /// - Space: O(1)
     pub fn div(&self, numer: u64) -> (u64, u32) {
         let quot = ((self.im as u128 * numer as u128) >> 64) as u64;
         let rem = (numer - quot * self.denom as u64) as u32;
