b2b34b80940a65541614ffc543e0a0d74627cf7c
Luke Videckis <lukevideckis@gmail.com>
2024-06-14 13:54:40 -0500

    differert var names

--- src/numbers/barrett.rs
+++ src/numbers/barrett.rs
@@ -1,24 +1,24 @@
 //! # Barrett Mod
 
 pub struct Barrett {
-    m: u32,
+    denom: u32,
     im: u64,
 }
 
 impl Barrett {
-    pub fn new(m: u32) -> Self {
-        assert_ne!(m, 0);
+    pub fn new(denom: u32) -> Self {
+        assert_ne!(denom, 0);
         Self {
-            m,
-            im: u64::MAX / (m as u64),
+            denom,
+            im: u64::MAX / (denom as u64),
         }
     }
 
-    pub fn div(&self, a: u64) -> (u64, u32) {
-        let quot = ((self.im as u128 * a as u128) >> 64) as u64;
-        let rem = (a - quot * self.m as u64) as u32;
-        if rem >= self.m {
-            (quot + 1, rem - self.m)
+    pub fn div(&self, numer: u64) -> (u64, u32) {
+        let quot = ((self.im as u128 * numer as u128) >> 64) as u64;
+        let rem = (numer - quot * self.denom as u64) as u32;
+        if rem >= self.denom {
+            (quot + 1, rem - self.denom)
         } else {
             (quot, rem)
         }
