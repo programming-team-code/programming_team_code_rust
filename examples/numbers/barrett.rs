e6374aee97dd1cec8831609e1f4626a89662875c
Luke Videckis <lukevideckis@gmail.com>
2024-06-14 14:13:17 -0500

    more asserts!!!

--- examples/numbers/barrett.rs
+++ examples/numbers/barrett.rs
@@ -3,14 +3,45 @@
 use proconio::input;
 use programming_team_code_rust::numbers::barrett::Barrett;
 
+fn do_asserts(numerator: u64, denominator: u32) {
+    let barr = Barrett::new(denominator);
+    let (quot, rem) = barr.div(numerator);
+    assert_eq!(quot * denominator as u64 + rem as u64, numerator);
+    assert!(rem < denominator);
+}
+
 fn main() {
     input! {
-        a: u32,
-        b: u32,
+        numerator: u32,
+        denominator: u32,
     }
-    let barr = Barrett::new(b);
 
-    let (quot, rem) = barr.div(a as u64);
+    let test_numerators = [
+        numerator as u64,
+        denominator as u64,
+        0_u64,
+        1_u64,
+        u64::MAX - 1,
+        u64::MAX,
+        numerator as u64 * denominator as u64,
+        numerator as u64 * numerator as u64,
+        denominator as u64 * denominator as u64,
+        numerator as u64 * 2_000_000_000_u64,
+        denominator as u64 * 2_000_000_000_u64,
+    ];
 
-    println!("{} {} {:.8}", quot, rem, (a as f64) / (b as f64));
+    for test_num in test_numerators {
+        do_asserts(test_num, numerator);
+        do_asserts(test_num, denominator);
+    }
+
+    let barr = Barrett::new(denominator);
+
+    let (quot, rem) = barr.div(numerator as u64);
+    println!(
+        "{} {} {:.8}",
+        quot,
+        rem,
+        (numerator as f64) / (denominator as f64)
+    );
 }
