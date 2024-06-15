6a33ce16d97ee0f7615f688c4ec55d283399fc2d
Luke Videckis <lukevideckis@gmail.com>
2024-06-14 22:33:53 -0500

    first draft

--- src/numbers/mod_int.rs
+++ src/numbers/mod_int.rs
@@ -1,5 +1,7 @@
 //! # Modulo Integer
 
+use crate::numbers::extended_gcd::ext_gcd;
+
 pub const MOD: u32 = 998_244_353;
 
 pub struct Mint {
@@ -8,38 +10,43 @@ pub struct Mint {
 
 impl Mint {
     pub fn new(mut val: i64) -> Self {
-        //TODO does val need to be mut?
-        let imod = MOD as i64;
-        if !(-imod..imod).contains(&val) {
-            val %= imod;
+        if !(-(MOD as i64)..2 * MOD as i64).contains(&val) {
+            val %= MOD as i64;
         }
-        if val < 0 {
-            val += imod;
+        if val >= MOD as i64 {
+            val -= MOD as i64;
+        } else if val < 0 {
+            val += MOD as i64;
         }
-        Mint { val: val as u32 }
+        Self { val: val as u32 }
     }
 
-    /*
-    pub fn pow(self) -> Self {
-    }
-
-    pub fn inv(self) -> Self {
-    }
-    */
-}
-
-impl std::fmt::Display for Mint {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}", self.val)
+    pub fn exp(self, mut e: u64) -> Self {
+        let mut res = Mint::new(1);
+        let mut b = Mint::new(self.val as i64);
+        while e > 0 {
+            if e % 2 == 0 {
+                res *= Mint::new(b.val as i64);
+            }
+            b *= Mint::new(b.val as i64);
+            e /= 2;
+        }
+        res
     }
 }
 
 impl std::ops::Add for Mint {
     type Output = Self;
     fn add(self, rhs: Self) -> Self {
-        let res = self.val + rhs.val;
-        Mint {
-            val: if res < MOD { res } else { res - MOD },
+        Mint::new((self.val + rhs.val) as i64)
+    }
+}
+
+impl std::ops::AddAssign for Mint {
+    fn add_assign(&mut self, rhs: Self) {
+        self.val += rhs.val;
+        if self.val >= MOD {
+            self.val -= MOD;
         }
     }
 }
@@ -47,9 +54,45 @@ impl std::ops::Add for Mint {
 impl std::ops::Sub for Mint {
     type Output = Self;
     fn sub(self, rhs: Self) -> Self {
-        let res = self.val + MOD - rhs.val;
-        Mint {
-            val: if res < MOD { res } else { res - MOD },
+        Mint::new((self.val + MOD - rhs.val) as i64)
+    }
+}
+
+impl std::ops::SubAssign for Mint {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.val += MOD - rhs.val;
+        if self.val >= MOD {
+            self.val -= MOD;
         }
+    }
+}
+
+impl std::ops::Mul for Mint {
+    type Output = Self;
+    fn mul(self, rhs: Self) -> Self {
+        Mint::new(self.val as i64 * rhs.val as i64)
+    }
+}
+
+impl std::ops::MulAssign for Mint {
+    fn mul_assign(&mut self, rhs: Self) {
+        self.val = ((self.val as i64) * (rhs.val as i64) % (MOD as i64)) as u32;
+    }
+}
+
+impl std::ops::Div for Mint {
+    type Output = Self;
+    fn div(self, rhs: Self) -> Self {
+        let (gcd, x, _) = ext_gcd(rhs.val as i64, MOD as i64);
+        assert_eq!(gcd, 1);
+        Mint::new(self.val as i64 * x)
+    }
+}
+
+impl std::ops::DivAssign for Mint {
+    fn div_assign(&mut self, rhs: Self) {
+        let (gcd, x, _) = ext_gcd(rhs.val as i64, MOD as i64);
+        assert_eq!(gcd, 1);
+        self.val = ((self.val as i64) * x % (MOD as i64)) as u32;
     }
 }
