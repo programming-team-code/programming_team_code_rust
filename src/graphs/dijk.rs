87632c894a0853c4bce18642ab3b9929d71fa61d
Luke Videckis <lukevideckis@gmail.com>
2024-06-15 23:47:37 -0500

    extra functionality for dijkstra

--- src/graphs/dijk.rs
+++ src/graphs/dijk.rs
@@ -4,19 +4,14 @@
 /// ```
 /// use programming_team_code_rust::graphs::dijk::dijk;
 ///
-/// let adj = vec![
-///    vec![(1, 1), (2, 4)],
-///    vec![(2, 2), (3, 5)],
-///    vec![(3, 1)],
-///    vec![],
-///    vec![(0, 7)],
-///    vec![(1, 3), (3, 2)],
-///    vec![(0, 2), (2, 3)],
-///    vec![(1, 2), (4, 2)],
-/// ];
+/// let mut adj = vec![vec![]; 4];
+/// for (u, v, w) in [(0, 1, 10), (1, 2, 100), (0, 2, 111), (2, 2, 0)] {
+///    adj[u].push((v, w));
+/// }
 ///
-/// let dist = dijk(&adj, 0);
-/// assert_eq!(dist, vec![0, 1, 3, 4, u64::MAX, u64::MAX, u64::MAX, u64::MAX]);
+/// let (dist, par) = dijk(&adj, 0);
+/// assert_eq!(dist, [0, 10, 110, u64::MAX]);
+/// assert_eq!(par, [None, Some(0), Some(1), None]);
 /// ```
 ///
 /// # Complexity
@@ -24,20 +19,21 @@
 /// - E: number of edges
 /// - Time: O(V + E log E)
 /// - Space: O(V + E)
-pub fn dijk(adj: &[Vec<(usize, u64)>], s: usize) -> Vec<u64> {
+pub fn dijk(adj: &[Vec<(usize, u64)>], s: usize) -> (Vec<u64>, Vec<Option<usize>>) {
     use std::cmp::Reverse;
     let n = adj.len();
     let mut dist = vec![u64::MAX; n];
+    let mut par = vec![None; n];
     let mut q = std::collections::BinaryHeap::new();
-    q.push(Reverse((0, s)));
-    while let Some(Reverse((d, u))) = q.pop() {
+    q.push(Reverse((0, s, None)));
+    while let Some(Reverse((d, u, p))) = q.pop() {
         if dist[u] <= d {
             continue;
         }
-        dist[u] = d;
+        (dist[u], par[u]) = (d, p);
         for &(v, w) in &adj[u] {
-            q.push(Reverse((dist[u] + w, v)));
+            q.push(Reverse((dist[u] + w, v, Some(u))));
         }
     }
-    dist
+    (dist, par)
 }
