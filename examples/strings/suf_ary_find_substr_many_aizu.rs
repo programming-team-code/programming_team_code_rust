1fc4325ca45c44faf0d059e4d146a26e33371e4e
Luke Videckis <lukevideckis@gmail.com>
2024-06-18 10:16:53 -0500

    speedup test

--- examples/strings/suf_ary_find_substr_many_aizu.rs
+++ examples/strings/suf_ary_find_substr_many_aizu.rs
@@ -10,11 +10,13 @@ fn main() {
         q: usize
     }
 
+    let num_queries_find_substr = q.min(100);
+
     let mut s = s.chars().map(|x| x as usize).collect::<Vec<usize>>();
 
     let mut length = vec![s.len()];
 
-    for _ in 0..q {
+    for _ in 0..num_queries_find_substr {
         input! {
             t: String,
         }
@@ -26,11 +28,25 @@ fn main() {
     let suf_ary = SufAry::new(&s, 255);
     let rmq = RMQ::new(&suf_ary.sa, std::cmp::min);
 
-    for i in 0..q {
+    for i in 0..num_queries_find_substr {
         let idx = rmq.query(suf_ary.find_substr(length[i]..length[i + 1]));
         println!(
             "{}",
             (idx + length[i + 1] - length[i] <= length[0]) as usize
         );
+    }
+
+    for _ in num_queries_find_substr..q {
+        input! {
+            t: String,
+        }
+        let t = t.chars().map(|x| x as usize).collect::<Vec<usize>>();
+        let match_range = suf_ary.find_str(&t);
+        let idx = if match_range.is_empty() {
+            *length.last().unwrap()
+        } else {
+            rmq.query(match_range)
+        };
+        println!("{}", (idx + t.len() <= length[0]) as usize);
     }
 }
