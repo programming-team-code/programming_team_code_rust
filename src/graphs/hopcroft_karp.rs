00cc846c0bf5ac7a27b81cd25bd23400f3f43c92
Luke Videckis <lrvideckis@gmail.com>
2024-06-20 10:13:09 -0500

    Bipartite matching (#65)
    * adding progress
    
    * finish yosupo test
    
    * nit
    
    * finish docs
    
    * fix code cov
    
    * another rename
    
    * move duplicate code to new file
    
    * I like this way more
    
    * add back other way as aizu test doesn't check actual matching
    
    * fix one nit
    
    * address another comment
    
    * add comment
    
    * remove duplicate code
    
    * convert
    
    * fix documentation
    
    * finish docs
    
    ---------
    
    Co-authored-by: Luke Videckis <lukevideckis@gmail.com>

---
+++ src/graphs/hopcroft_karp.rs
@@ -0,0 +1,112 @@
+//! # Hopcroft Karp algorithm for Bipartite Matching
+
+use std::collections::VecDeque;
+
+/// # Example
+/// ```
+/// use programming_team_code_rust::graphs::hopcroft_karp::HopcroftKarp;
+///
+/// let mut adj = vec![vec![]; 2];
+/// for (u, v) in [(0, 0), (0, 2), (1, 2)] {
+///    adj[u].push(v);
+/// }
+///
+/// let HopcroftKarp {
+///    matching_siz,
+///    l_to_r,
+///    r_to_l,
+///    mvc_l,
+///    mvc_r,
+/// } = HopcroftKarp::new(&adj, 3);
+///
+/// assert_eq!(matching_siz, 2);
+/// assert_eq!(l_to_r, [Some(0), Some(2)]);
+/// assert_eq!(r_to_l, [Some(0), None, Some(1)]);
+/// assert_eq!(mvc_l, [true, true]);
+/// assert_eq!(mvc_r, [false, false, false]);
+/// ```
+///
+/// # Complexity
+/// - V: number of vertices; V = lsz + rsz
+/// - E: number of edges
+/// - Time: O(V + E * sqrt(v))
+/// - Space: O(V)
+pub struct HopcroftKarp {
+    /// number of edges in matching
+    pub matching_siz: usize,
+    /// l_to_r\[u_left_side\] = Some(v_right_side) iff edge u_left_side <=> v_right_side is in matching
+    pub l_to_r: Vec<Option<usize>>,
+    /// r_to_l\[v_right_side\] = Some(u_left_side) iff edge u_left_side <=> v_right_side is in matching
+    pub r_to_l: Vec<Option<usize>>,
+    /// mvc_l\[u_left_side\] = true iff u_left_side is in min vertex cover
+    pub mvc_l: Vec<bool>,
+    /// mvc_r\[v_right_side\] = true iff v_right_side is in min vertex cover
+    pub mvc_r: Vec<bool>,
+}
+
+impl HopcroftKarp {
+    /// Calculates a max matching and min vertex cover
+    pub fn new(adj: &[Vec<usize>], rsz: usize) -> Self {
+        let lsz = adj.len();
+        let mut e = HopcroftKarp {
+            matching_siz: 0,
+            l_to_r: vec![None; lsz],
+            r_to_l: vec![None; rsz],
+            mvc_l: vec![false; lsz],
+            mvc_r: vec![false; rsz],
+        };
+        loop {
+            let mut dist = vec![usize::MAX; lsz];
+            let mut q = VecDeque::new();
+            for (i, _) in e
+                .l_to_r
+                .iter()
+                .enumerate()
+                .filter(|&(_, elem)| elem.is_none())
+            {
+                dist[i] = 0;
+                q.push_back(i);
+            }
+            let mut found = false;
+            for v in &mut e.mvc_l {
+                *v = true;
+            }
+            for v in &mut e.mvc_r {
+                *v = false;
+            }
+            while let Some(u) = q.pop_front() {
+                e.mvc_l[u] = false;
+                for &v in &adj[u] {
+                    e.mvc_r[v] = true;
+                    if let Some(w) = e.r_to_l[v] {
+                        if dist[w] > 1 + dist[u] {
+                            dist[w] = 1 + dist[u];
+                            q.push_back(w);
+                        }
+                    } else {
+                        found = true;
+                    }
+                }
+            }
+            if !found {
+                return e;
+            }
+            fn dfs(u: usize, adj: &[Vec<usize>], dist: &mut [usize], e: &mut HopcroftKarp) -> bool {
+                for &v in &adj[u] {
+                    let w = e.r_to_l[v];
+                    if w.is_none()
+                        || dist[u] + 1 == dist[w.unwrap()] && dfs(w.unwrap(), adj, dist, e)
+                    {
+                        (e.l_to_r[u], e.r_to_l[v]) = (Some(v), Some(u));
+                        return true;
+                    }
+                }
+                dist[u] = usize::MAX;
+                false
+            }
+            e.matching_siz += (0..lsz)
+                .filter(|&i| e.l_to_r[i].is_none() && dfs(i, adj, &mut dist, &mut e))
+                .count();
+        }
+    }
+}
