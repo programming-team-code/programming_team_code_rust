f3244910abf14b09929c4372b15f2f5c30dfffc4
Luke Videckis <lukevideckis@gmail.com>
2024-06-19 21:36:54 -0500

    fix

--- src/graphs/bridges.rs
+++ src/graphs/bridges.rs
@@ -28,36 +28,34 @@ use crate::helpers::recursive_fnmut::recursive_fnmut;
 pub fn get_bridges(adj: &[Vec<(usize, usize)>], m: usize) -> (usize, Vec<bool>, Vec<usize>) {
     let (n, mut timer, mut num_2_edge_ccs, mut is_bridge) = (adj.len(), 1, 0, vec![false; m]);
     let (mut tin, mut two_edge_ccid, mut st) = (vec![0; n], vec![0; n], Vec::with_capacity(n));
-
-    let mut dfs = recursive_fnmut!(|dfs, u: usize, p_id: Option<usize>| -> usize {
-        tin[u] = timer;
-        let (mut low, st_sz) = (timer, st.len());
-        timer += 1;
-        st.push(u);
-        for &(v, e_id) in &adj[u] {
-            if Some(e_id) == p_id {
-                continue;
-            }
-            if tin[v] == 0 {
-                low = low.min(dfs(v, Some(e_id)));
-            }
-            low = low.min(tin[v]);
-        }
-        if tin[u] == low {
-            if let Some(p) = p_id {
-                is_bridge[p] = true;
-            }
-            for &id in st.iter().skip(st_sz) {
-                two_edge_ccid[id] = num_2_edge_ccs;
-            }
-            st.truncate(st_sz);
-            num_2_edge_ccs += 1;
-        }
-        low
-    });
-
     for i in 0..n {
         if tin[i] == 0 {
+            let mut dfs = recursive_fnmut!(|dfs, u: usize, p_id: Option<usize>| -> usize {
+                tin[u] = timer;
+                let (mut low, st_sz) = (timer, st.len());
+                timer += 1;
+                st.push(u);
+                for &(v, e_id) in &adj[u] {
+                    if Some(e_id) == p_id {
+                        continue;
+                    }
+                    if tin[v] == 0 {
+                        low = low.min(dfs(v, Some(e_id)));
+                    }
+                    low = low.min(tin[v]);
+                }
+                if tin[u] == low {
+                    if let Some(p) = p_id {
+                        is_bridge[p] = true;
+                    }
+                    for &id in st.iter().skip(st_sz) {
+                        two_edge_ccid[id] = num_2_edge_ccs;
+                    }
+                    st.truncate(st_sz);
+                    num_2_edge_ccs += 1;
+                }
+                low
+            });
             dfs(i, None);
         }
     }
