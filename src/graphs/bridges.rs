93deb9be3b78b446592e024bfb2fedeb152c6e30
Luke Videckis <lrvideckis@gmail.com>
2024-06-20 09:40:33 -0500

    another idea to clean up dfs (#73)
    * another idea to clean up dfs
    
    * different style
    
    * convert bridges to new style
    
    * hld example refactor
    
    * Revert "hld example refactor"
    
    This reverts commit e5a77a11a45f3380bd1301e5aebd821264be1254.
    
    ---------
    
    Co-authored-by: Luke Videckis <lukevideckis@gmail.com>

--- src/graphs/bridges.rs
+++ src/graphs/bridges.rs
@@ -24,69 +24,53 @@
 /// - Time: O(V + E)
 /// - Space: O(V)
 pub fn get_bridges(adj: &[Vec<(usize, usize)>], m: usize) -> (usize, Vec<bool>, Vec<usize>) {
-    #[allow(clippy::too_many_arguments)]
-    fn dfs(
-        u: usize,
-        p_id: Option<usize>,
-        adj: &[Vec<(usize, usize)>],
-        timer: &mut usize,
-        tin: &mut [usize],
-        num_2_edge_ccs: &mut usize,
-        is_bridge: &mut [bool],
-        two_edge_ccid: &mut [usize],
-        st: &mut Vec<usize>,
-    ) -> usize {
-        tin[u] = *timer;
-        let (mut low, st_sz) = (*timer, st.len());
-        *timer += 1;
-        st.push(u);
+    let n = adj.len();
+    struct Env {
+        timer: usize,
+        tin: Vec<usize>,
+        num_2_edge_ccs: usize,
+        is_bridge: Vec<bool>,
+        two_edge_ccid: Vec<usize>,
+        st: Vec<usize>,
+    }
+    let mut e = Env {
+        timer: 1,
+        tin: vec![0; n],
+        num_2_edge_ccs: 0,
+        is_bridge: vec![false; m],
+        two_edge_ccid: vec![0; n],
+        st: Vec::with_capacity(n),
+    };
+    fn dfs(e: &mut Env, u: usize, p_id: Option<usize>, adj: &[Vec<(usize, usize)>]) -> usize {
+        e.tin[u] = e.timer;
+        let (mut low, st_sz) = (e.timer, e.st.len());
+        e.timer += 1;
+        e.st.push(u);
         for &(v, e_id) in &adj[u] {
             if Some(e_id) == p_id {
                 continue;
             }
-            if tin[v] == 0 {
-                low = low.min(dfs(
-                    v,
-                    Some(e_id),
-                    adj,
-                    timer,
-                    tin,
-                    num_2_edge_ccs,
-                    is_bridge,
-                    two_edge_ccid,
-                    st,
-                ));
+            if e.tin[v] == 0 {
+                low = low.min(dfs(e, v, Some(e_id), adj));
             }
-            low = low.min(tin[v]);
+            low = low.min(e.tin[v]);
         }
-        if tin[u] == low {
+        if e.tin[u] == low {
             if let Some(p) = p_id {
-                is_bridge[p] = true;
+                e.is_bridge[p] = true;
             }
-            for &id in st.iter().skip(st_sz) {
-                two_edge_ccid[id] = *num_2_edge_ccs;
+            for &id in e.st.iter().skip(st_sz) {
+                e.two_edge_ccid[id] = e.num_2_edge_ccs;
             }
-            st.truncate(st_sz);
-            *num_2_edge_ccs += 1;
+            e.st.truncate(st_sz);
+            e.num_2_edge_ccs += 1;
         }
         low
     }
-    let (n, mut timer, mut num_2_edge_ccs, mut is_bridge) = (adj.len(), 1, 0, vec![false; m]);
-    let (mut tin, mut two_edge_ccid, mut st) = (vec![0; n], vec![0; n], Vec::with_capacity(n));
     for i in 0..n {
-        if tin[i] == 0 {
-            dfs(
-                i,
-                None,
-                adj,
-                &mut timer,
-                &mut tin,
-                &mut num_2_edge_ccs,
-                &mut is_bridge,
-                &mut two_edge_ccid,
-                &mut st,
-            );
+        if e.tin[i] == 0 {
+            dfs(&mut e, i, None, adj);
         }
     }
-    (num_2_edge_ccs, is_bridge, two_edge_ccid)
+    (e.num_2_edge_ccs, e.is_bridge, e.two_edge_ccid)
 }
