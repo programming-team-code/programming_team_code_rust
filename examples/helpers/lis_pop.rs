e23589344c60531b55cae2f096c07c8d0ff67942
Luke Videckis <lukevideckis@gmail.com>
2024-06-22 15:51:06 -0500

    about to add another test

---
+++ examples/helpers/lis_pop.rs
@@ -0,0 +1,9 @@
+// verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/all/ITP1_1_A
+
+use programming_team_code_rust::helpers::lis::Lis;
+
+fn main() {
+    //    let mut lis = Lis::default();
+
+    println!("Hello World");
+}
