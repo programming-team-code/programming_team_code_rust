d84085ddc0bdc2a25e05a9fc6c577afe39127adf
Luke Videckis <lukevideckis@gmail.com>
2024-06-26 18:17:37 -0500

    nit

--- examples/strings/suf_ary_find_str_aizu.rs
+++ examples/strings/suf_ary_find_str_aizu.rs
@@ -9,11 +9,11 @@ fn main() {
         t: String
     }
 
-    let s_vec = s.chars().map(|x| x as usize).collect::<Vec<usize>>();
-    let t_vec = t.chars().map(|x| x as usize).collect::<Vec<usize>>();
+    let s = s.chars().map(|x| x as usize).collect::<Vec<usize>>();
+    let t = t.chars().map(|x| x as usize).collect::<Vec<usize>>();
 
-    let suf_ary = SufAry::new(&s_vec, 255);
-    let range = suf_ary.find_str(&t_vec);
+    let suf_ary = SufAry::new(&s, 255);
+    let range = suf_ary.find_str(&t);
     let mut res: Vec<usize> = Vec::new();
     res.extend_from_slice(&suf_ary.sa[range]);
     res.sort();
