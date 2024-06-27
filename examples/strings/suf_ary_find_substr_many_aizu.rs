c288b9f60b0f0b734c8eaf20f9da3aabb7fa16da
Luke Videckis <lukevideckis@gmail.com>
2024-06-26 20:06:01 -0500

    now call function from other test

--- examples/strings/suf_ary_find_substr_many_aizu.rs
+++ examples/strings/suf_ary_find_substr_many_aizu.rs
@@ -4,6 +4,9 @@ use proconio::input;
 use programming_team_code_rust::data_structures::rmq::RMQ;
 use programming_team_code_rust::strings::suf_ary::SufAry;
 
+mod suf_ary_push_pop_substr_asserts;
+use suf_ary_push_pop_substr_asserts::suf_ary_push_pop_substr_asserts;
+
 fn main() {
     input! {
         s: String,
@@ -29,7 +32,11 @@ fn main() {
     let rmq = RMQ::new(&suf_ary.sa, std::cmp::min);
 
     for i in 0..num_queries_find_substr {
-        let idx = rmq.query(suf_ary.find_substr(length[i]..length[i + 1]));
+        let s_to_look_for_range = length[i]..length[i + 1];
+        let range = suf_ary.find_substr(s_to_look_for_range.clone());
+        suf_ary_push_pop_substr_asserts(s.len(), &suf_ary, &range, &s_to_look_for_range);
+
+        let idx = rmq.query(range);
         println!(
             "{}",
             (idx + length[i + 1] - length[i] <= length[0]) as usize
