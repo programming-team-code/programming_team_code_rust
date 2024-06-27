b91a922ae740e7e828964a6f9d6516519b55b232
Luke Videckis <lukevideckis@gmail.com>
2024-06-26 19:29:51 -0500

    simplify test

--- examples/strings/suf_ary_cmp_aizu.rs
+++ examples/strings/suf_ary_cmp_aizu.rs
@@ -7,26 +7,23 @@ use programming_team_code_rust::strings::suf_ary::SufAry;
 fn main() {
     input! {
         n: usize,
-        mut a: [i32; n],
+        mut a: [usize; n],
         m: usize,
-        b: [i32; m]
+        b: [usize; m]
     }
 
     let are_equal = a == b;
 
-    a.push(-1);
     a.extend(b);
 
     let (cmps, max_val) = compress(&a);
     let suf_ary = SufAry::new(&cmps, max_val);
-    let res = suf_ary.cmp_substrs(0..n, n + 1..n + 1 + m);
+    let res = suf_ary.cmp_substrs(0..n, n..n + m);
 
     if are_equal {
         assert_eq!(res, std::cmp::Ordering::Equal);
-        assert_eq!(suf_ary.cmp_sufs(0, n + 1), std::cmp::Ordering::Greater);
         println!("0");
     } else {
-        assert_eq!(res, suf_ary.cmp_sufs(0, n + 1));
         println!("{}", (res == std::cmp::Ordering::Less) as usize);
     }
 }
