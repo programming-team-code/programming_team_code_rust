70cd60cd71b81959a0c8e8c88a116808804e52a3
Luke Videckis <lukevideckis@gmail.com>
2024-06-29 13:28:41 -0500

    saving progress

---
+++ examples/helpers/interval_container.rs
@@ -0,0 +1,11 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/longest_increasing_subsequence
+
+use proconio::input;
+use programming_team_code_rust::helpers::interval_contiainer::IntervalContiainer;
+
+fn main() {
+    input! {
+        n: usize,
+        q: usize,
+    }
+}
--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -1,4 +1,5 @@
 //! # Helpers
 pub mod compress;
+pub mod interval_container;
 pub mod lis;
 pub mod unsafe_recursive_closure;
