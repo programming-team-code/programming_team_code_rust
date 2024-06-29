7bf6a6280b3293b96a8e75530211cb730a6f4819
Luke Videckis <lukevideckis@gmail.com>
2024-06-29 14:44:16 -0500

    finished test, need to fix some bugs tho

--- examples/helpers/range_container.rs
+++ examples/helpers/range_container.rs
@@ -1,12 +1,41 @@
-// verification-helper: PROBLEM https://judge.yosupo.jp/problem/longest_increasing_subsequence
+// verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/problems/DSL_2_D
 
 use proconio::input;
 use programming_team_code_rust::helpers::range_container::RangeContainer;
+use std::collections::BTreeMap;
 
 fn main() {
     input! {
         n: usize,
         q: usize,
     }
-    let mut ic = RangeContainer::default();
+    let mut rc = RangeContainer::default();
+    let mut to_value = BTreeMap::<i32, u32>::new();
+    rc.insert_range(0..n as i32);
+    to_value.insert(0, 0);
+
+    for _ in 0..q {
+        input! {
+            kind: usize,
+        }
+        match kind {
+            0 => {
+                input! {
+                    le: i32,
+                    ri: i32,
+                    x: u32,
+                }
+                rc.remove_range(le - 1..ri + 1);
+                rc.insert_range(le..ri);
+                to_value.insert(le, x);
+            }
+            _ => {
+                input! {
+                    index: i32,
+                }
+                let key = rc.mp.range(..=index).next_back().unwrap().0;
+                println!("{}", to_value.get(key).unwrap());
+            }
+        }
+    }
 }
