d7e72403d69670685ff4a9aa52ec1f163e39e98e
Luke Videckis <lukevideckis@gmail.com>
2024-07-03 11:33:23 -0500

    beef up test

--- examples/monotonic/count_rects.rs
+++ examples/monotonic/count_rects.rs
@@ -14,6 +14,34 @@ fn main() {
         .map(|row| row.iter().map(|&num| num == 0).collect::<Vec<_>>())
         .collect::<Vec<_>>();
     let cnt = count_rects(&grid);
+
+    {
+        let mut pref_sum = vec![vec![0; m + 1]; n + 1];
+        for i in 0..n {
+            for j in 0..m {
+                pref_sum[i + 1][j + 1] =
+                    grid[i][j] as i64 + pref_sum[i + 1][j] + pref_sum[i][j + 1] - pref_sum[i][j];
+            }
+        }
+        for len_i in 1..=10.min(n) {
+            for len_j in 1..10.min(m) {
+                let mut cnt_good_rects = 0;
+                for i in 0..=n - len_i {
+                    for j in 0..=m - len_j {
+                        let cnt_rect = pref_sum[i + len_i][j + len_j]
+                            - pref_sum[i + len_i][j]
+                            - pref_sum[i][j + len_j]
+                            + pref_sum[i][j];
+                        if cnt_rect == len_i as i64 * len_j as i64 {
+                            cnt_good_rects += 1;
+                        }
+                    }
+                }
+                assert_eq!(cnt_good_rects, cnt[len_i][len_j]);
+            }
+        }
+    }
+
     let mut res = 0;
     for (i, row) in cnt.iter().enumerate().skip(1) {
         for (j, &num) in row.iter().enumerate().skip(1) {
