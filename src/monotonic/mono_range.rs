fc29bdfbf3b1e4ec3f738f16d18d2cae2dd6e863
Luke Videckis <lukevideckis@gmail.com>
2024-07-03 10:30:02 -0500

    consistency with c++ PTC

--- src/monotonic/mono_range.rs
+++ src/monotonic/mono_range.rs
@@ -1,7 +1,6 @@
 pub fn mono_range(le: &[usize]) -> Vec<usize> {
-    let n = le.len();
-    let mut ri = vec![n; n];
-    for i in 0..n {
+    let mut ri = vec![le.len(); le.len()];
+    for i in 0..le.len() {
         let mut j = i.wrapping_sub(1);
         while j != le[i] {
             ri[j] = i;
