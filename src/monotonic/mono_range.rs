cbb0b8284110ff1377a41c5e84e7f25228aabf1e
Luke Videckis <lukevideckis@gmail.com>
2024-07-03 10:50:50 -0500

    add docs for mono range

--- src/monotonic/mono_range.rs
+++ src/monotonic/mono_range.rs
@@ -1,3 +1,22 @@
+//! # Monotonic Range
+
+/// Gets vec ri where ri[i] = smallese index such that i < ri[i] && !a[i].cmp(&a[ri[i]]), or n
+///
+/// # Example
+/// ```
+/// use programming_team_code_rust::monotonic::mono_st::mono_st;
+/// use programming_team_code_rust::monotonic::mono_range::mono_range;
+///
+/// let a: Vec<u32> = vec![3, 1, 2, 2];
+///
+/// let le = mono_st(&a, |x, y| x.lt(y));
+/// let ri = mono_range(&le);
+/// assert_eq!(ri, [1, 4, 3, 4]);
+/// ```
+///
+/// # Complexity
+/// - Time: O(n)
+/// - Space: O(n)
 pub fn mono_range(le: &[usize]) -> Vec<usize> {
     let mut ri = vec![le.len(); le.len()];
     for i in 0..le.len() {
