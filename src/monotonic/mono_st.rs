00acb23b25839435865ddbdda357be2b67684107
Luke Videckis <lukevideckis@gmail.com>
2024-07-03 10:45:06 -0500

    add

--- src/monotonic/mono_st.rs
+++ src/monotonic/mono_st.rs
@@ -1,16 +1,20 @@
 //! # Monotonic Stack
 
+/// Gets vec le where le[i] = largest index such that le[i] < i && a[le[i]].cmp(&a[i]), or usize::MAX
+///
 /// # Example
 /// ```
 /// use programming_team_code_rust::monotonic::mono_st::mono_st;
 ///
 /// let a: Vec<u32> = vec![3, 1, 2, 2];
 /// let n = a.len();
+///
 /// assert_eq!(mono_st(&a, |x, y| x.lt(y)), [usize::MAX, usize::MAX, 1, 1]);
 /// assert_eq!(mono_st(&a, |x, y| x.le(y)), [usize::MAX, usize::MAX, 1, 2]);
 /// assert_eq!(mono_st(&a, |x, y| x.gt(y)), [usize::MAX, 0, 0, 0]);
 /// assert_eq!(mono_st(&a, |x, y| x.ge(y)), [usize::MAX, 0, 0, 2]);
 ///
+/// // simulate popping off stack for single index
 /// let le = mono_st(&a, |x, y| x.lt(y));
 /// let mut seen_index = vec![0; n];
 /// for i in 0..n {
