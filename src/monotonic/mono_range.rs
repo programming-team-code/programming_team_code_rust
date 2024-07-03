35a88ad2d56b4239b9638281ec35921406ee8235
Luke Videckis <lukevideckis@gmail.com>
2024-07-03 10:14:24 -0500

    nit

--- src/monotonic/mono_range.rs
+++ src/monotonic/mono_range.rs
@@ -1,8 +1,8 @@
 pub fn mono_range(le: &[usize]) -> Vec<usize> {
     let n = le.len();
     let mut ri = vec![n; n];
-    for i in 1..n {
-        let mut j = i - 1;
+    for i in 0..n {
+        let mut j = i.wrapping_sub(1);
         while j != le[i] {
             ri[j] = i;
             j = le[j];
