45e974e64611967fa205ed4a8cacacb8b6ae143c
Luke Videckis <lukevideckis@gmail.com>
2024-07-04 10:27:00 -0500

    format

--- src/monotonic/count_rects.rs
+++ src/monotonic/count_rects.rs
@@ -14,7 +14,8 @@ use crate::monotonic::mono_st::mono_st;
 /// ```
 /// use programming_team_code_rust::monotonic::count_rects::count_rects;
 ///
-/// let grid = [vec![true, true, false], vec![true, true, true]];
+/// let grid = [vec![true, true, false],
+///             vec![true, true, true]];
 ///
 /// // remove garbage values
 /// let mut cnt = count_rects(&grid);
@@ -22,7 +23,8 @@ use crate::monotonic::mono_st::mono_st;
 /// for i in 0..2 {
 ///    cnt[i].drain(0..1);
 /// }
-/// assert_eq!(cnt, [[5, 3, 1], [2, 1, 0]]);
+/// assert_eq!(cnt, [[5, 3, 1],
+///                  [2, 1, 0]]);
 /// ```
 ///
 /// # Complexity
