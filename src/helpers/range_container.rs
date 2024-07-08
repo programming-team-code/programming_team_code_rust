b09ec38093f59371c1e8b3735c4ee153f37c3a76
Luke Videckis <lukevideckis@gmail.com>
2024-07-08 16:45:24 -0500

    finished docs

--- src/helpers/range_container.rs
+++ src/helpers/range_container.rs
@@ -1,13 +1,30 @@
-//! # Interval Container
+//! # Range Container
 
 use std::collections::BTreeMap;
 use std::ops::Range;
 
+/// type of endpoints of ranges
 pub type T = i32;
 
+/// # Example
+/// ```
+/// use programming_team_code_rust::helpers::range_container::RangeContainer;
+///
+/// let mut rc = RangeContainer::default();
+/// rc.insert_range(-2..2);
+/// rc.insert_range(2..3);
+/// assert_eq!(rc.get_range(0), Some(-2..3));
+/// assert_eq!(rc.get_range(3), None);
+/// rc.remove_range(-1..1);
+///
+/// assert_eq!(rc.get_range(-2), Some(-2..-1));
+/// assert_eq!(rc.get_range(2), Some(1..3));
+/// ```
 #[derive(Default)]
 pub struct RangeContainer {
-    mp: BTreeMap<T, T>,
+    /// an entry le -> ri represents a range [le, ri)
+    /// invariant: previous_ri < current_le
+    pub mp: BTreeMap<T, T>,
 }
 
 impl RangeContainer {
@@ -25,6 +42,11 @@ impl RangeContainer {
         last_ri
     }
 
+    /// Inserts a range into the container
+    ///
+    /// # Complexity
+    /// - Time: O(log n) ammortized
+    /// - Space: O(1) ammortized
     pub fn insert_range(&mut self, mut range: Range<T>) {
         if let Some(last_ri) = self.remove(&range) {
             range.end = std::cmp::max(range.end, last_ri);
@@ -38,6 +60,11 @@ impl RangeContainer {
         self.mp.insert(range.start, range.end);
     }
 
+    /// Removes a range from the container
+    ///
+    /// # Complexity
+    /// - Time: O(log n) ammortized
+    /// - Space: O(1) ammortized
     pub fn remove_range(&mut self, range: Range<T>) {
         if let Some(last_ri) = self.remove(&range) {
             if range.end < last_ri {
@@ -52,6 +79,11 @@ impl RangeContainer {
         }
     }
 
+    /// Gets range containing idx
+    ///
+    /// # Complexity
+    /// - Time: O(log n) ammortized
+    /// - Space: O(1)
     pub fn get_range(&self, idx: T) -> Option<Range<T>> {
         if let Some((&le, &ri)) = self.mp.range(..=idx).next_back() {
             if idx < ri {
