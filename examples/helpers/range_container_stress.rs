d0f6ed46eaa2cf8344dee176983cba1431fb98a4
Luke Videckis <lukevideckis@gmail.com>
2024-07-09 12:41:13 -0500

    nits

---
+++ examples/helpers/range_container_stress.rs
@@ -0,0 +1,10 @@
+// verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/all/ITP1_1_A
+
+use programming_team_code_rust::helpers::range_container::RangeContainer;
+
+fn main() {
+    let max_n = rand::random::<u32>();
+    let mut rc = RangeContainer::default();
+    let mut
+    println!("Hello World");
+}
