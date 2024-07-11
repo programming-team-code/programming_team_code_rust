a2a4474359901d0c9882a523c08e3e352c168f2c
Luke Videckis <lukevideckis@gmail.com>
2024-07-11 13:41:08 -0500

    converted another

--- src/data_structures/rmq.rs
+++ src/data_structures/rmq.rs
@@ -5,14 +5,14 @@
 /// use programming_team_code_rust::data_structures::rmq::RMQ;
 ///
 /// let a = [1, 3, 2, 4, 5];
-/// let rmq1 = RMQ::new(&a, std::cmp::min);
-/// assert_eq!(rmq1.query(0..5), 1);
-/// assert_eq!(rmq1.query(1..4), 2);
+/// let rmq = RMQ::new(&a, std::cmp::min);
+/// assert_eq!(rmq.query(0..5), 1);
+/// assert_eq!(rmq.query(1..4), 2);
 ///
 /// let outside_var = 5;
-/// let rmq2 = RMQ::new(&a, |x, y| if x + outside_var < y + outside_var { x } else { y });
-/// assert_eq!(rmq2.query(0..5), 1);
-/// assert_eq!(rmq2.query(1..4), 2);
+/// let rmq = RMQ::new(&a, |x, y| if x + outside_var < y + outside_var { x } else { y });
+/// assert_eq!(rmq.query(0..5), 1);
+/// assert_eq!(rmq.query(1..4), 2);
 /// ```
 pub struct RMQ<T, F> {
     t: Vec<Vec<T>>,
