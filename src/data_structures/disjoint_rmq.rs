f16c6447058c7fc094b6afd8882b64d12d14bae7
Luke Videckis <lukevideckis@gmail.com>
2024-07-11 15:03:47 -0500

    first draft of disjoint rmq

--- src/data_structures/disjoint_rmq.rs
+++ src/data_structures/disjoint_rmq.rs
@@ -5,18 +5,48 @@ pub struct DisjointRMQ<T, F> {
     op: F,
 }
 
-impl<T: Copy, F: Fn(T, T) -> T> DisjointRMQ<T, F> {
+impl<T: Clone, F: Fn(&T, &T) -> T> DisjointRMQ<T, F> {
     /// Create a new Disjoint RMQ instance
     ///
     /// # Complexity (n = a.len())
     /// - Time: O(n log n)
     /// - Space: O(n log n)
-    pub fn new(a: &[T], op: F) -> Self {}
+    pub fn new(a: &[T], op: F) -> Self {
+        let mut t = vec![];
+        let mut len = 1;
+        while len <= a.len() {
+            let mut le = 0;
+            let mut row = a.to_vec();
+            while le < a.len() {
+                let mi = a.len().min(le + len);
+                let ri = a.len().min(le + 2 * len);
+                for i in (le..mi - 1).rev() {
+                    row[i] = op(&row[i], &row[i + 1]);
+                }
+                if mi < ri {
+                    for i in mi + 1..ri {
+                        row[i] = op(&row[i - 1], &row[i]);
+                    }
+                }
+                le += 2 * len;
+            }
+            t.push(row);
+            len *= 2;
+        }
+        Self { t, op }
+    }
 
     /// Query the range [range.start, range.end)
     ///
     /// # Complexity
     /// - Time: O(1)
     /// - Space: O(1)
-    pub fn query(&self, range: std::ops::Range<usize>) -> T {}
+    pub fn query(&self, range: std::ops::Range<usize>) -> T {
+        if range.len() == 1 {
+            self.t[0][range.start].clone()
+        } else {
+            let lg = (range.start ^ (range.end - 1)).ilog2() as usize;
+            (self.op)(&self.t[lg][range.start], &self.t[lg][range.end - 1])
+        }
+    }
 }
