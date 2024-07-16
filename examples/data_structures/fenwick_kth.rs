43248daff0db7a5d3b722476135f6a2d786e184d
Luke Videckis <lrvideckis@gmail.com>
2024-07-16 12:45:21 -0500

    Bit walk (#88)
    * bit walk
    
    * add example
    
    * different interface actually
    
    * fix docs
    
    * Update fenwick.rs
    
    ---------
    
    Co-authored-by: Luke Videckis <lukevideckis@gmail.com>

---
+++ examples/data_structures/fenwick_kth.rs
@@ -0,0 +1,54 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/predecessor_problem
+
+use proconio::input;
+use programming_team_code_rust::data_structures::fenwick::Fenwick;
+
+fn main() {
+    input! {
+        n: usize,
+        q: usize,
+        s: String,
+    }
+
+    let mut fenwick =
+        Fenwick::<i32>::build_on_array(&s.chars().map(|c| (c == '1') as i32).collect::<Vec<_>>());
+
+    for _ in 0..q {
+        input! {
+            t: u8,
+            k: usize,
+        }
+        match t {
+            0 => {
+                if fenwick.sum(k..k + 1) == 0 {
+                    fenwick.add(k, 1);
+                }
+            }
+            1 => {
+                if fenwick.sum(k..k + 1) == 1 {
+                    fenwick.add(k, -1);
+                }
+            }
+            2 => {
+                println!("{}", fenwick.sum(k..k + 1));
+            }
+            3 => {
+                let cnt = fenwick.sum(0..k);
+                let res = fenwick.kth(cnt + 1);
+                if res == n {
+                    println!("-1");
+                } else {
+                    println!("{}", res);
+                }
+            }
+            _ => {
+                let cnt_le = fenwick.sum(0..k + 1);
+                if cnt_le == 0 {
+                    println!("-1");
+                } else {
+                    println!("{}", fenwick.kth(cnt_le));
+                }
+            }
+        }
+    }
+}
