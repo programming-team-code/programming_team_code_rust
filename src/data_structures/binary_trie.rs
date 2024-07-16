55cd88b8cb3f32fcb9e574ad29e02651d192ff1a
Luke Videckis <lrvideckis@gmail.com>
2024-07-16 10:31:49 -0500

    Bin trie nits (#86)
    * type nits
    
    * another nit
    
    * added docs
    
    * change type here too
    
    ---------
    
    Co-authored-by: Luke Videckis <lukevideckis@gmail.com>
    Co-authored-by: Cameron Custer <73217097+cameroncuster@users.noreply.github.com>

--- src/data_structures/binary_trie.rs
+++ src/data_structures/binary_trie.rs
@@ -1,9 +1,12 @@
 //! # Binary Trie which can be used as a multiset of integers
 
+/// u32 or u64
+pub type T = u32;
+
 #[derive(Default)]
 struct Node {
     next: [Option<usize>; 2],
-    sub_sz: isize,
+    sub_sz: i32,
 }
 
 /// # Example
@@ -44,10 +47,10 @@ impl BinaryTrie {
     /// # Complexity
     /// - Time: O(log(max_num))
     /// - Space: O(log(max_num))
-    pub fn update(&mut self, num: usize, delta: isize) {
+    pub fn update(&mut self, num: T, delta: i32) {
         let mut v = 0;
-        for i in (0..usize::BITS).rev() {
-            let bit = (num >> i) & 1;
+        for i in (0..T::BITS).rev() {
+            let bit = ((num >> i) & 1) as usize;
             if self.t[v].next[bit].is_none() {
                 self.t[v].next[bit] = Some(self.t.len());
                 self.t.push(Node::default());
@@ -63,14 +66,15 @@ impl BinaryTrie {
     /// # Complexity
     /// - Time: O(log(max_num))
     /// - Space: O(1)
-    pub fn count(&self, num: usize) -> isize {
+    pub fn count(&self, num: T) -> i32 {
         let mut v = 0;
-        for i in (0..usize::BITS).rev() {
-            let bit = (num >> i) & 1;
-            if self.t[v].next[bit].is_none() {
+        for i in (0..T::BITS).rev() {
+            let bit = ((num >> i) & 1) as usize;
+            if let Some(u) = self.t[v].next[bit] {
+                v = u;
+            } else {
                 return 0;
             }
-            v = self.t[v].next[bit].unwrap();
         }
         self.t[v].sub_sz
     }
@@ -80,12 +84,12 @@ impl BinaryTrie {
     /// # Complexity
     /// - Time: O(log(max_num))
     /// - Space: O(1)
-    pub fn min_xor(&self, num: usize) -> usize {
-        assert!(self.t.len() > 1);
+    pub fn min_xor(&self, num: T) -> T {
+        assert!(self.t[0].sub_sz > 0);
         let mut v = 0;
         let mut ans = 0;
-        for i in (0..usize::BITS).rev() {
-            let bit = (num >> i) & 1;
+        for i in (0..T::BITS).rev() {
+            let bit = ((num >> i) & 1) as usize;
             if self.t[v].next[bit].is_some() && self.t[self.t[v].next[bit].unwrap()].sub_sz > 0 {
                 v = self.t[v].next[bit].unwrap();
             } else {
